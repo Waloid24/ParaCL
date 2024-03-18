2188894ce50db0a40a55722812d2a17639d1cbbe
Vasily Markov <mvv1993Vasya@gmail.com>
2024-03-18 15:17:08 +0300

    Evaluate singl arithe expressions

--- ast.hpp
+++ ast.hpp
@@ -25,13 +25,13 @@ public:
     virtual void dump() const = 0;
     virtual ~iNode(){}
 
-    friend void eval(std::shared_ptr<iNode> node) {
-        if (node == nullptr) return;
-
-        eval(node->left_);
-        node->dump();
-        eval(node->right_);
-    }
+    virtual double eval() const = 0;
+    // friend double dump(std::shared_ptr<iNode> node) {
+    //     if (node == nullptr) return 0.0;
+    //     eval(node->left_);
+    //     node->dump();
+    //     eval(node->left_);         
+    // }
 };
 
 class opNode: public iNode { 
@@ -40,6 +40,23 @@ public:
     opNode(op_t op, std::shared_ptr<iNode> left, std::shared_ptr<iNode> right): 
         op_(op), 
         iNode(left, right) {}
+    double eval() const override {
+        double left_value = left_ ? left_->eval(): 0.0;
+        double right_value = right_ ? right_->eval(): 0.0;
+
+        switch (op_)
+        {
+            case op_t::PLUS:
+                return left_value + right_value;
+            break;
+            case op_t::MINUS:
+                return left_value - right_value;
+            break;
+            default:
+                return 0.0;
+            break;
+        }
+    }
     void dump() const override {
         std::string name;
         switch (op_)
@@ -57,17 +74,25 @@ public:
     }
 };
 
-
 class numNode: public iNode { 
     int value_;
 public:
     numNode(int value): 
         value_(value), 
         iNode(nullptr, nullptr) {}
+    double eval() const override {
+        return static_cast<double>(value_); 
+    }
     void dump() const override {
         std::cout << "Number: " << value_ << std::endl;
     }
 };
+
+inline double eval(std::shared_ptr<iNode> node) {
+    if (node == nullptr)
+        return 0.0; 
+    return node->eval(); 
+}
 
 inline std::shared_ptr<iNode> newOp(op_t type, std::shared_ptr<iNode> left, std::shared_ptr<iNode> right) {
     return std::make_shared<opNode>(type, left, right);
--- grammar-sr.y
+++ grammar-sr.y
@@ -52,7 +52,7 @@ statement_list: statement
 
 statement: expr SCOLON
       {
-        eval(head);     
+        std::cout << eval(head) << std::endl;     
       }
 ;
 
